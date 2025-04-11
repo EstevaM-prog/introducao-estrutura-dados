f14938db5ddc398da0f71c2cbe7022f444c2984b
EstevaM-prog <estevamx9@gmail.com>
2025-04-11 13:43:13 -0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let alisson_nota_av1 = 8;
+let alisson_nota_av2 = 9.6;
+let alisson_media = (alisson_nota_av1 + alisson_nota_av2) / 2
+
+console.log ('Média Alison;' + alisson_media);
+
+console.log ('Exemplo com arrays')
+let notas_alison = [8, 9.6];
+console.log ('notas_alison');
+
+console.log('AV1: '+notas_alison[0]);
+console.log('AV2: '+notas_alison[0]);
+notas_alison[2] = (notas_alison[0] + notas_alison[1]) / 2;
+console.log ('Media: ' +notas_alison[2]);
+console.log ('Todas: ' +notas_alison);
